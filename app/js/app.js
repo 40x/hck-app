4427692af7392e3715dd4be4fcf9b579c2193416
Kashyap Mukkamala <kashyap.mukkamala@gmail.com>
2015-06-05 19:06:42 -0500

    add ui router

--- app/js/app.js
+++ app/js/app.js
@@ -1,40 +1,21 @@
+(function(){
 
 
-    var app = angular.module('application', ['ngRoute', 'homePage']);
+    var app = angular.module('application', ['ui.router']);
 
-    var homePage = angular.module('homePage', ['ngRoute']);
 
-    app.config(['$routeProvider', function ($routeProvider){
-        $routeProvider
-            .when('/home',
-            {
+    app.config(function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+
+            .state('home', {
+                url: '/home',
                 controller: 'homeController',
                 templateUrl: 'partials/home.html'
-            })
-            .when('/landing',
-            {
-                controller: 'landingController',
-                templateUrl: 'partials/landing.html'
-            })
-            .when('/notifications',
-            {
-                controller: 'notificationController',
-                templateUrl: 'partials/notifications.html'
-            })
-            .when('/profile',
-            {
-                controller: 'profileController',
-                templateUrl: 'partials/profifle.html'
-            })
-            .when('/settings',
-            {
-                controller: 'settingsController',
-                templateUrl: 'partials/settings.html'
-            })
-            .when('/activity',
-            {
-                controller: 'activityController',
-                templateUrl: 'partials/activity.html'
-            })
-            .otherwise({redirectTo: '/home'});
-    }]);
+            });
+
+    });
+
+})();
